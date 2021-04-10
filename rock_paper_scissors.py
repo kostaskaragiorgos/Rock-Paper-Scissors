70aeb4ced8c7ef32375fa2f840a00a28b0c4c7b0
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-04-10 14:29:41 +0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,5 @@
-from tkinter import Menu, Button, StringVar, OptionMenu, messagebox as msg, filedialog, Tk, Label
-from tkinter import simpledialog, scrolledtext , WORD
+from tkinter import Menu, Button, StringVar, OptionMenu, messagebox as msg, Tk, Label
+from tkinter import simpledialog, scrolledtext, WORD
 from random import choice
 import logging
 
@@ -9,49 +9,50 @@ class RockPaperScissors():
         self.master.title("ROCK PAPER SCISSORS")
         self.master.geometry("250x300")
         self.master.resizable(False, False)
-        self.score = [0,0]
+        self.score = [0, 0]
         self.choices = ["", ""]
         self.numberofrounds = 0
 
         self.roundslabel = Label(self.master, text="Number of rounds: " +str(self.numberofrounds))
         self.roundslabel.pack()
 
-        self.scorelabel = Label(self.master, text="Score: "+str(self.score[0])+"-"+str(self.score[1]))
+        self.scorelabel = Label(self.master,
+                                text="Score: "+str(self.score[0])+"-"+str(self.score[1]))
         self.scorelabel.pack()
 
 
         self.category_list = list(["Rock", "Paper", "Scissors"])
         self.var_cat_list = StringVar(master)
         self.var_cat_list.set(self.category_list[0])
-        self.popupcatlistmenu = OptionMenu(self.master, self.var_cat_list,   *self.category_list)
+        self.popupcatlistmenu = OptionMenu(self.master, self.var_cat_list, *self.category_list)
         self.popupcatlistmenu.pack()
 
         self.playb = Button(self.master, text="Play", state='disabled', command=self.play)
         self.playb.pack()
 
         self.log_area = scrolledtext.ScrolledText(self.master,
-                                             wrap = WORD,
-                                             width = 40,
-                                             height = 10,
-                                             font = ("Times New Roman", 15))
+                                                  wrap=WORD,
+                                                  width=40,
+                                                  height=10,
+                                                  font=("Times New Roman", 15))
         self.log_area.configure(state='disabled')
         self.log_area.pack()
 
         
         self.menu = Menu(self.master)
         
-        self.file_menu = Menu(self.menu, tearoff = 0)
+        self.file_menu = Menu(self.menu, tearoff=0)
         self.file_menu.add_command(label="New Game", accelerator='Ctrl+O', command=self.newgame)
         self.file_menu.add_command(label="Load Game")
-        self.file_menu.add_command(label="Exit", accelerator='Alt+F4', command = self.exitmenu)
-        self.menu.add_cascade(label = "File", menu=self.file_menu)
+        self.file_menu.add_command(label="Exit", accelerator='Alt+F4', command=self.exitmenu)
+        self.menu.add_cascade(label="File", menu=self.file_menu)
         
-        self.about_menu = Menu(self.menu, tearoff = 0)
-        self.about_menu.add_command(label = "About", accelerator= 'Ctrl+I', command=self.aboutmenu)
+        self.about_menu = Menu(self.menu, tearoff=0)
+        self.about_menu.add_command(label="About", accelerator='Ctrl+I', command=self.aboutmenu)
         self.menu.add_cascade(label="About", menu=self.about_menu)
         
-        self.help_menu = Menu(self.menu, tearoff = 0)
-        self.help_menu.add_command(label = "Help", accelerator = 'Ctrl+F1', command=self.helpmenu)
+        self.help_menu = Menu(self.menu, tearoff=0)
+        self.help_menu.add_command(label="Help", accelerator='Ctrl+F1', command=self.helpmenu)
         self.menu.add_cascade(label="Help", menu=self.help_menu)
         
         self.master.config(menu=self.menu)
@@ -84,7 +85,7 @@ class RockPaperScissors():
         #self.log_area.configure(state='normal')
         #self.log_area.insert("insert",chars = logger1.info("Round"+ str(self.numberofrounds) + "Player" + str(self.score[0]) + "Computer" + str(self.score[1])))
         #self.log_area.configure(state='disabled')
-        self.scorelabel['text'] ="Score: "+str(self.score[0])+"-"+str(self.score[1])
+        self.scorelabel['text'] = "Score: "+str(self.score[0])+"-"+str(self.score[1])
         self.roundslabel['text'] = "Number of rounds: " +str(self.numberofrounds)
         if self.numberofrounds == 0:
             self.playb.configure(state='disabled')
@@ -98,9 +99,9 @@ class RockPaperScissors():
 
     def newgame(self):
         self.numberofrounds = simpledialog.askinteger("Number of rounds",
-                                                          "Enter the number of rounds",
-                                                          parent=self.master,
-                                                          minvalue=1)
+                                                      "Enter the number of rounds",
+                                                      parent=self.master,
+                                                      minvalue=1)
         while self.numberofrounds == 0 or self.numberofrounds is None:
             self.numberofrounds = simpledialog.askinteger("Number of rounds",
                                                           "Enter the number of rounds",
@@ -109,7 +110,7 @@ class RockPaperScissors():
         self.roundslabel['text'] = "Number of rounds: " +str(self.numberofrounds)
         self.score[0] = 0
         self.score[1] = 0
-        self.scorelabel['text'] ="Score: "+str(self.score[0])+"-"+str(self.score[1])
+        self.scorelabel['text'] = "Score: "+str(self.score[0])+"-"+str(self.score[1])
         self.playb.configure(state='normal')
         
 
@@ -124,9 +125,9 @@ class RockPaperScissors():
         pass
 
 def main():
-    root=Tk()
+    root = Tk()
     RockPaperScissors(root)
     root.mainloop()
     
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
